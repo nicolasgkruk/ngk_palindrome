c41589eb9245abe404aef5abefcef6a71a3cec86
“nicolasgkruk” <nicolasgkruk@gmail.com>
2019-01-04 11:29:22 +0100

    added integer support

--- lib/ngk_palindrome.rb
+++ lib/ngk_palindrome.rb
@@ -1,22 +1,28 @@
 require "ngk_palindrome/version"
 
-class String
+module NgkPalindrome
 
   # Returns true for a palindrome, false otherwise.
   def palindrome?
-    processed_content == processed_content.reverse
-  end
-
-  # Returns the letters in the string.
-  def letters
-
+    if processed_content.empty?
+      false
+    else
+      processed_content == processed_content.reverse
+    end
   end
 
   private
 
     # Returns content for palindrome testing.
     def processed_content
-      #self.scan(/[a-z]/i).join.downcase -> original version provided by Michael Hartl in Learn Enough Tutorial.
-      self.gsub(/\W/, '').downcase
+      self.to_s.scan(/[A-Za-z0-9]/).join.downcase
     end
+end
+
+class String
+  include NgkPalindrome
+end
+
+class Integer
+  include NgkPalindrome
 end
--- test/ngk_palindrome_test.rb
+++ test/ngk_palindrome_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class MhartlPalindromeTest < Minitest::Test
+class NgkPalindromeTest < Minitest::Test
 
   def test_non_palindrome
     refute "apple".palindrome?
@@ -20,5 +20,13 @@ class MhartlPalindromeTest < Minitest::Test
 
   def test_palindrome_with_palindromic_phrase
     assert "A man, a plan, a canal—Panama!"
+  end
+
+  def test_integer_non_palindrome
+    refute 12345.palindrome?
+  end
+
+  def test_integer_palindrome
+    assert 12321.palindrome?
   end
 end
