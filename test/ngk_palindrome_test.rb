c8d09ea81f6f903a6ea7e25b995013e67c82316c
“nicolasgkruk” <nicolasgkruk@gmail.com>
2019-01-04 10:32:39 +0100

    tests and basic functionality added

--- test/ngk_palindrome_test.rb
+++ test/ngk_palindrome_test.rb
@@ -1,11 +1,12 @@
 require "test_helper"
 
 class NgkPalindromeTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::NgkPalindrome::VERSION
+
+  def test_non_palindrome
+    assert !"apple".palindrome?
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_literal_palindrome
+    assert "racecar".palindrome?
   end
 end
